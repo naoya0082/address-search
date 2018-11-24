59b2164f63700ee3026ea3bf95fef43d7d84f3af
naoya0082 <6ds3f7@gmail.com>
2018-11-24 17:30:07 +0900

    app.py に移動した(リファクタリング)

--- test-address-seacher.py
+++ test-address-seacher.py
@@ -1,28 +1,6 @@
 import unittest
-import requests
 
-
-class AddressSearcher:
-    def search(self, postal_code):
-        url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
-        response = requests.get(url)
-        response_dict = response.json()
-
-        都道府県 = response_dict["results"][0]["address1"]
-        市区町村 = response_dict["results"][0]["address2"]
-        町域 = response_dict["results"][0]["address3"]
-        return f"{都道府県}{市区町村}{町域}"
-
-    # def search2(self, postal_code):
-    #     url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=1760014"
-    #     response = requests.get(url)
-    #     response_dict = response.json()
-    #
-    #     都道府県 = response_dict["results"][0]["address1"]
-    #     市区町村 = response_dict["results"][0]["address2"]
-    #     町域 = response_dict["results"][0]["address3"]
-    #     return f"{都道府県}{市区町村}{町域}"
-
+from app import AddressSearcher
 
 
 class TestAddressSearcher(unittest.TestCase):
