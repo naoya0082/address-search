56881ff6362b005c2f75db02c7100e6d0599d124
naoya0082 <6ds3f7@gmail.com>
2018-11-24 16:32:45 +0900

    これからユーザーの入力によって地名を検索する仕組みを作る

--- test-address-seacher.py
+++ test-address-seacher.py
@@ -13,6 +13,12 @@ class TestAddressSearcher(unittest.TestCase):
         actual = address_searcher.search(postal_code="0287111")
         self.assertEqual("岩手県八幡平市大更", actual)
 
+    def test_東京都練馬区豊玉南の地名を郵便番号から取得(self):
+        address_searcher = AddressSearcher()
+
+        actual = address_searcher.search(postal_code="1760014")
+        self.assertEqual("東京都練馬区豊玉南", actual)
+
 
 if __name__ == "__main__":
     unittest.main()
