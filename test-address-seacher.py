620247eb3c7597517c0b3fb5cef09ed6b2d0bb0f
naoya0082 <6ds3f7@gmail.com>
2018-11-24 16:12:26 +0900

    岩手県八幡平市を取得できるテストを書いた

--- test-address-seacher.py
+++ test-address-seacher.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class TestAddressSearcher(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_岩手県八幡平市大更の地名を郵便番号から取得(self):
+        address_searcher = AddressSearcher()
+
+        actual = address_searcher.search(postal_code="0287111")
+        self.assertEqual("岩手県八幡平市大更", actual)
 
 
 if __name__ == "__main__":
