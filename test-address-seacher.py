d5ebff2f992fafca6cb4778de06624a6a9187575
naoya0082 <6ds3f7@gmail.com>
2018-11-24 17:18:09 +0900

    岩手県と東京都のテストを通した

--- test-address-seacher.py
+++ test-address-seacher.py
@@ -1,9 +1,28 @@
 import unittest
+import requests
 
 
 class AddressSearcher:
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
+        response = requests.get(url)
+        response_dict = response.json()
+
+        都道府県 = response_dict["results"][0]["address1"]
+        市区町村 = response_dict["results"][0]["address2"]
+        町域 = response_dict["results"][0]["address3"]
+        return f"{都道府県}{市区町村}{町域}"
+
+    # def search2(self, postal_code):
+    #     url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=1760014"
+    #     response = requests.get(url)
+    #     response_dict = response.json()
+    #
+    #     都道府県 = response_dict["results"][0]["address1"]
+    #     市区町村 = response_dict["results"][0]["address2"]
+    #     町域 = response_dict["results"][0]["address3"]
+    #     return f"{都道府県}{市区町村}{町域}"
+
 
 
 class TestAddressSearcher(unittest.TestCase):
