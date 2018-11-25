fe8faa0fa34f499bc73044885fa9f908201e08fb
naoya0082 <6ds3f7@gmail.com>
2018-11-25 09:05:25 +0900

    存在しない郵便番号を入力した際のエラーメッセージを追加中

--- test-address-seacher.py
+++ test-address-seacher.py
@@ -17,6 +17,11 @@ class TestAddressSearcher(unittest.TestCase):
         actual = address_searcher.search(postal_code="1760014")
         self.assertEqual("東京都練馬区豊玉南", actual)
 
+    def test_存在しない郵便番号が入力されたらエラーメッセージを表示する(self):
+        address_searcher = AddressSearcher()
+        actual = address_searcher.search(postal_code="1234567")
+        expected = "該当するデータは見つかりませんでした。検索キーワードを変えて再検索してください。"
+        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
